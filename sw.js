245c7b54fc0a8b059ad9970dbad691df82782ab8
MichelV <62699812+MichelVGameMaker@users.noreply.github.com>
2025-09-09 20:08:28 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'muscu-app-v4';
+const CORE = [
+  './','./index.html','./styles.css?v=4','./app.js?v=4','./db.js','./manifest.webmanifest?v=4'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if(url.origin===location.origin){
+    e.respondWith(
+      caches.match(e.request).then(r=> r || fetch(e.request).then(res=>{
+        const copy = res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request, copy));
+        return res;
+      }))
+    );
+  }
+});
