ec7aeb47fa96ec1fe8640c403b75dcb1cff199f7
MichelV <62699812+MichelVGameMaker@users.noreply.github.com>
2026-05-05 16:42:39 +0200

    Ajuste le layout du clavier inline en modes edit/timer

--- components/set-editor.js
+++ components/set-editor.js
@@ -640,7 +640,7 @@
             integer: ['1',  '2',  '3',  '4',   '5', '6',   '7',    '8',   '9',     null,  '0',  'del'],
             rpe:     ['5',  '5.5', '6', '6.5', '7', '7.5', '8',    '8.5', '9',     '9.5', '10', '-'],
             time:    ['1',  '2',  '3',  '4',   '5', '6',   '7',    '8',   '9',     ':',   '0',  'del'],
-            timer:   ['timerDisplay', 'timerReset', 'done', '-10', 'timerToggle', '+10', null, null, 'close'],
+            timer:   ['timerDisplay', 'timerReset', null, '-10', 'timerToggle', '+10'],
             edit:    ['trash', 'up', null, 'down', null, null]
         };
 
